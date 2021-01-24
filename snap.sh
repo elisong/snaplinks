492ea2514362e355baa02704aa60543b5678e607
Eli Song <elisong.ah@gmail.com>
2021-01-24 16:34:22 +0800

    v1.0.2

--- snap.sh
+++ snap.sh
@@ -1,6 +1,6 @@
 while read kw; do
     mkdir -p  "$kw"
     cd "$kw"
-    google "$kw" --num 20 --output-file "$(date '+%Y-%m-%d')"
+    google "$kw" --num 2 --output-file "$(date '+%Y-%m-%d')"
     cd ..
 done <keywords.txt
