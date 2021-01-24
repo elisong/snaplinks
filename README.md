e36c8970e7a614dea005e0980e219290d21d628f
Eli Song <elisong.ah@gmail.com>
2021-01-24 15:55:24 +0800

    v1.0

---
+++ README.md
@@ -0,0 +1,5 @@
+# Snapshot google links for certain keyword day by day
+
+for example:
+
+`quant` => google `quant` and return top 20 page links
--- snap.sh
+++ snap.sh
@@ -1,6 +1,6 @@
 while read kw; do
     mkdir -p  $kw
     cd $kw
-    google $kw --site github.com --num 20 -o "$(date '+%Y-%m-%d').md"
+    google $kw --num 20 -o "$(date '+%Y-%m-%d')"
     cd ..
 done <keywords.txt
